71cac9dbc11bde62b8e1a1e48acf1f0437f40ef8
Andrew Appleby <andrewappleby22@gmail.com>
2020-02-01 00:30:14 -0800

    Updates

---
+++ precache-manifest.17d9cb2dbb0e2962198414853050cc76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0983139c24d0c60acc4ffa0157d9ccb",
+    "url": "/celery/index.html"
+  },
+  {
+    "revision": "b229ae5d14d206d22454",
+    "url": "/celery/static/js/2.99460c44.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/celery/static/js/2.99460c44.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff9e9edf3d12a53c1f9c",
+    "url": "/celery/static/js/main.ce4da264.chunk.js"
+  },
+  {
+    "revision": "fcb5a295c24a7ad6e6e7",
+    "url": "/celery/static/js/runtime-main.588135d2.js"
+  },
+  {
+    "revision": "8ccebf470b107f53cb88d3e1c6cc7a5b",
+    "url": "/celery/static/media/CeleryIcon.8ccebf47.svg"
+  }
+]);
